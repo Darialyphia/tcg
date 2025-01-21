ad5146151fb44d4b0adf91248a2b9bfb6bdac91f
Loic Pennequin <lpennequin@samsys.fr>
2025-01-21 17:48:09 +0100

    bleh

---
+++ packages/engine/src/game/game-phase.system.ts
@@ -0,0 +1,8 @@
+import { StateMachine } from '@game/shared';
+import { System } from '../system';
+
+export class GamePhaseSystem extends System<{}> {
+  initialize(options: {}): void {}
+
+  shutdown() {}
+}
